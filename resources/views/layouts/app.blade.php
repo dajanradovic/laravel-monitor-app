b8ec937eb0134a3d97ad58ee236b200278621db7
Dajan Radovic <dajo1986@gmail.com>
2020-05-07 20:04:45 +0200

    fake alerts removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -314,7 +314,7 @@ footer{
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }} ({{Auth::user()->department}}) <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -494,7 +494,7 @@ function markAsRead(notificationId){
 
 
 function markMessageAsRead(notificationId){
-    alert('horny');
+    
     axios.get('../../markMessageAsRead?id='+ notificationId , {
                     
                     })
